54e250b1006c33e56dc6440ba0ba6f61f37aad1b
Eugeniosag <eugenio.sanchezg@uanl.edu.mx>
2026-04-20 08:22:50 -0600

    se creo la clase Consultorio

---
+++ Evidencia3/CodigoEvidencia3.py
@@ -0,0 +1,8 @@
+class Consultorio:
+    def __init__(self,id_consultorio,sucursal,piso,medico):
+        self.__id_consultorio=id_consultorio
+        self.__sucursal=sucursal
+        self.__piso=piso
+        self.__medico=medico
+        self.__medicamentos=[]
+        self.__pacientes=[]
